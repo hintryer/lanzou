0d87040bf5d92fd2885949098b9f7a13ed90ca39
hintryer <gindeny+github@gmail.com>
2026-04-11 13:47:02 +0800

    Update lan.py

--- lan.py
+++ lan.py
@@ -4,3 +4,5 @@ lzy = LanZouCloud()
 cookie = {'ylogin': '984259', 'phpdisk_info': 'WGVVbwVjBzlQZwNqXQ5bNVUMUWIMYFw5AzIBZwU7UGJXZlFmAmdWbAcMAGNdMVBrBm4EMlw2UDYHNQViAzMHNVg9VWEFbwdvUDADMV02WzJVNlEyDDZcPgNiAWYFO1BqVzJRMQI1VmsHMQBZXTZQPwZkBDRcM1A%2FBzQFYAMwBzJYag%3D%3D'}
 print(lzy.login_by_cookie(cookie) == LanZouCloud.SUCCESS)
 # can get True
+folders = lzy.get_move_folders()
+print(folders)
