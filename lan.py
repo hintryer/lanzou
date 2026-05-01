2d246464751d28f6359927c5386c5810d655d26b
hintryer <gindeny+github@gmail.com>
2026-05-01 11:07:37 +0800

    Update lan.py

--- lan.py
+++ lan.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     variables = run_checkver(config["checkver"])
     data = fetch_data("https://api.github.com/repos/chenhb23/lanzouyun-disk/releases/latest")
-    print("提取结果：", data)
+    # print("提取结果：", data)
     txt = extract_value(data, "$..browser_download_url[?@ =~ /[^/]+\\.zip$/]")
     print("下载结果：", txt)
-    print(jsonpath.findall( "$.checkver[0].~",config))
+    # print(jsonpath.findall( "$.checkver[0].~",config))
