e9bdc428df7cd2a301a979ce94086ee1fd6df0b2
hintryer <19499425+hintryer@users.noreply.github.com>
2026-05-30 02:02:49 +0000

    Apply automatic changes

---
+++ download_release.bat
@@ -0,0 +1,151 @@
+@echo off
+chcp 65001 >nul
+cls
+echo ==============================================
+echo        从 GitHub Release 批量下载
+echo        版本：v20260530
+echo ==============================================
+echo.
+md soft 2>nul
+echo 📥 下载：微软常用运行库合集_2026.04.16.exe
+curl -L -# -o "soft\微软常用运行库合集_2026.04.16.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/微软常用运行库合集_2026.04.16.exe"
+echo.
+echo 📥 下载：FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip
+curl -L -# -o "soft\FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip"
+echo.
+echo 📥 下载：Chrome++_v1.16.2_x86_x64_arm64.7z
+curl -L -# -o "soft\Chrome++_v1.16.2_x86_x64_arm64.7z" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Chrome++_v1.16.2_x86_x64_arm64.7z"
+echo.
+echo 📥 下载：dotnet-runtime-8.0.27-win-x64.exe
+curl -L -# -o "soft\dotnet-runtime-8.0.27-win-x64.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/dotnet-runtime-8.0.27-win-x64.exe"
+echo.
+echo 📥 下载：EmEditor_portable.zip
+curl -L -# -o "soft\EmEditor_portable.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/EmEditor_portable.zip"
+echo.
+echo 📥 下载：dotnet-runtime-6.0.36-win-x64.exe
+curl -L -# -o "soft\dotnet-runtime-6.0.36-win-x64.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/dotnet-runtime-6.0.36-win-x64.exe"
+echo.
+echo 📥 下载：Notepad4_HD_zh-Hans_x64_v26.05r6166.zip
+curl -L -# -o "soft\Notepad4_HD_zh-Hans_x64_v26.05r6166.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Notepad4_HD_zh-Hans_x64_v26.05r6166.zip"
+echo.
+echo 📥 下载：dotnet-runtime-7.0.20-win-x64.exe
+curl -L -# -o "soft\dotnet-runtime-7.0.20-win-x64.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/dotnet-runtime-7.0.20-win-x64.exe"
+echo.
+echo 📥 下载：FSCapture_11.2.zip
+curl -L -# -o "soft\FSCapture_11.2.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/FSCapture_11.2.zip"
+echo.
+echo 📥 下载：解除网页复制限制_2.1.6.crx
+curl -L -# -o "soft\解除网页复制限制_2.1.6.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/解除网页复制限制_2.1.6.crx"
+echo.
+echo 📥 下载：Scribe2SRT-v0.3.6-windows-x86_64.zip
+curl -L -# -o "soft\Scribe2SRT-v0.3.6-windows-x86_64.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Scribe2SRT-v0.3.6-windows-x86_64.zip"
+echo.
+echo 📥 下载：Steamcommunity_302_AMD64_V14.0.02.zip
+curl -L -# -o "soft\Steamcommunity_302_AMD64_V14.0.02.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Steamcommunity_302_AMD64_V14.0.02.zip"
+echo.
+echo 📥 下载：lanzouyun-3.5.3-x64-win.zip
+curl -L -# -o "soft\lanzouyun-3.5.3-x64-win.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/lanzouyun-3.5.3-x64-win.zip"
+echo.
+echo 📥 下载：LastPass_4.153.1.crx
+curl -L -# -o "soft\LastPass_4.153.1.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/LastPass_4.153.1.crx"
+echo.
+echo 📥 下载：dotnet-runtime-9.0.16-win-x64.exe
+curl -L -# -o "soft\dotnet-runtime-9.0.16-win-x64.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/dotnet-runtime-9.0.16-win-x64.exe"
+echo.
+echo 📥 下载：ffmpeg-8.1.1-full_build.7z
+curl -L -# -o "soft\ffmpeg-8.1.1-full_build.7z" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/ffmpeg-8.1.1-full_build.7z"
+echo.
+echo 📥 下载：typora-plugin@v1.18.1.zip
+curl -L -# -o "soft\typora-plugin@v1.18.1.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/typora-plugin@v1.18.1.zip"
+echo.
+echo 📥 下载：GeekUninstaller_1.5.3.170.zip
+curl -L -# -o "soft\GeekUninstaller_1.5.3.170.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/GeekUninstaller_1.5.3.170.zip"
+echo.
+echo 📥 下载：QQWubi_Setup_2.4.629.400.exe
+curl -L -# -o "soft\QQWubi_Setup_2.4.629.400.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/QQWubi_Setup_2.4.629.400.exe"
+echo.
+echo 📥 下载：Clash.Verge_2.5.1_x64-setup.exe
+curl -L -# -o "soft\Clash.Verge_2.5.1_x64-setup.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Clash.Verge_2.5.1_x64-setup.exe"
+echo.
+echo 📥 下载：app_asar_file_v1.9.5.zip
+curl -L -# -o "soft\app_asar_file_v1.9.5.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/app_asar_file_v1.9.5.zip"
+echo.
+echo 📥 下载：РotРlayer-260422(1.7.22859)-x64-Stable.exe
+curl -L -# -o "soft\РotРlayer-260422(1.7.22859)-x64-Stable.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/РotРlayer-260422(1.7.22859)-x64-Stable.exe"
+echo.
+echo 📥 下载：SingleFile_1.22.98.crx
+curl -L -# -o "soft\SingleFile_1.22.98.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/SingleFile_1.22.98.crx"
+echo.
+echo 📥 下载：360FileSetup_1.0.0.1180.exe
+curl -L -# -o "soft\360FileSetup_1.0.0.1180.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/360FileSetup_1.0.0.1180.exe"
+echo.
+echo 📥 下载：PortableGit-2.54.0-64-bit.7z.exe
+curl -L -# -o "soft\PortableGit-2.54.0-64-bit.7z.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/PortableGit-2.54.0-64-bit.7z.exe"
+echo.
+echo 📥 下载：typora-windows-x64-1.9.5.exe
+curl -L -# -o "soft\typora-windows-x64-1.9.5.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/typora-windows-x64-1.9.5.exe"
+echo.
+echo 📥 下载：篡改猴测试版_5.5.6237.crx
+curl -L -# -o "soft\篡改猴测试版_5.5.6237.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/篡改猴测试版_5.5.6237.crx"
+echo.
+echo 📥 下载：SwitchyOmega_3.5.0.crx
+curl -L -# -o "soft\SwitchyOmega_3.5.0.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/SwitchyOmega_3.5.0.crx"
+echo.
+echo 📥 下载：ClickMonitorDDC.zip
+curl -L -# -o "soft\ClickMonitorDDC.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/ClickMonitorDDC.zip"
+echo.
+echo 📥 下载：超级拖拽_2.1.2.crx
+curl -L -# -o "soft\超级拖拽_2.1.2.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/超级拖拽_2.1.2.crx"
+echo.
+echo 📥 下载：LocalSend-1.17.0-windows-x86-64.zip
+curl -L -# -o "soft\LocalSend-1.17.0-windows-x86-64.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/LocalSend-1.17.0-windows-x86-64.zip"
+echo.
+echo 📥 下载：ffmpeg-8.1.1-essentials_build.7z
+curl -L -# -o "soft\ffmpeg-8.1.1-essentials_build.7z" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/ffmpeg-8.1.1-essentials_build.7z"
+echo.
+echo 📥 下载：沉浸式翻译_1.29.6.crx
+curl -L -# -o "soft\沉浸式翻译_1.29.6.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/沉浸式翻译_1.29.6.crx"
+echo.
+echo 📥 下载：MouseClickTool.exe
+curl -L -# -o "soft\MouseClickTool.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/MouseClickTool.exe"
+echo.
+echo 📥 下载：uBlockOriginLite_2026.516.1652.crx
+curl -L -# -o "soft\uBlockOriginLite_2026.516.1652.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/uBlockOriginLite_2026.516.1652.crx"
+echo.
+echo 📥 下载：MusicPlayer2_V2.78_x64.zip
+curl -L -# -o "soft\MusicPlayer2_V2.78_x64.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/MusicPlayer2_V2.78_x64.zip"
+echo.
+echo 📥 下载：chrome_148.0.7778.217_uncompressed.exe
+curl -L -# -o "soft\chrome_148.0.7778.217_uncompressed.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/chrome_148.0.7778.217_uncompressed.exe"
+echo.
+echo 📥 下载：Umi-OCR_Paddle_v2.1.5.7z.exe
+curl -L -# -o "soft\Umi-OCR_Paddle_v2.1.5.7z.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Umi-OCR_Paddle_v2.1.5.7z.exe"
+echo.
+echo 📥 下载：Thunder_12.4.9.3918.exe
+curl -L -# -o "soft\Thunder_12.4.9.3918.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Thunder_12.4.9.3918.exe"
+echo.
+echo 📥 下载：ReNamerPro_7.9.0.0v2.exe
+curl -L -# -o "soft\ReNamerPro_7.9.0.0v2.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/ReNamerPro_7.9.0.0v2.exe"
+echo.
+echo 📥 下载：猫抓_2.6.9.crx
+curl -L -# -o "soft\猫抓_2.6.9.crx" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/猫抓_2.6.9.crx"
+echo.
+echo 📥 下载：MusicTag.zip
+curl -L -# -o "soft\MusicTag.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/MusicTag.zip"
+echo.
+echo 📥 下载：dotnet-runtime-10.0.8-win-x64.exe
+curl -L -# -o "soft\dotnet-runtime-10.0.8-win-x64.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/dotnet-runtime-10.0.8-win-x64.exe"
+echo.
+echo 📥 下载：FSViewer_8.3.exe
+curl -L -# -o "soft\FSViewer_8.3.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/FSViewer_8.3.exe"
+echo.
+echo 📥 下载：VideoCaptioner-Setup-win64-v1.3.3.exe
+curl -L -# -o "soft\VideoCaptioner-Setup-win64-v1.3.3.exe" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/VideoCaptioner-Setup-win64-v1.3.3.exe"
+echo.
+echo 📥 下载：Bili23-Downloade_v2.00.6_portable.zip
+curl -L -# -o "soft\Bili23-Downloade_v2.00.6_portable.zip" "https://gh-proxy.com/https://github.com/hintryer/lanzou/releases/download/v20260530/Bili23-Downloade_v2.00.6_portable.zip"
+echo.
+echo ==============================================
+echo                ✅ 下载完成
+echo ==============================================
+pause
