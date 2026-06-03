58020933ed29daceba1b79e731508b65fd11625b
hintryer <gindeny@gmail.com>
2026-06-03 14:50:21 +0800

    456

--- download.bat
+++ download.bat
@@ -5,53 +5,77 @@ echo ==============================================
 echo           批量下载工具（自动分类）
 echo ==============================================
 echo.
-mkdir "运行库" 2>nul
-echo 📥 下载 → 运行库 \ All_VC_2026.04.16.exe
-curl -L -# -o "运行库\All_VC_2026.04.16.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQWtFanNtZ1J6cFM2S3dxRTNBOGhrekFjZHFsN19adTZtVTFCTVM2eVF3QVA0P2U9Q0UzOVpW.exe"
+mkdir "其它工具" 2>nul
+echo 📥 下载 → 其它工具 \ 360FileSetup_1.0.0.1180.exe
+curl -L -# -o "其它工具\360FileSetup_1.0.0.1180.exe" "https://sfdl.360safe.com/pcnewapp/360FileSetup.exe?channel=9510140"
 echo.
-mkdir "影音工具" 2>nul
-echo 📥 下载 → 影音工具 \ FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip
-curl -L -# -o "影音工具\FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip" "https://gh-proxy.com/https://github.com/eibols/ffmpeg_batch/releases/download/3.2.9/FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip"
+mkdir "其它工具" 2>nul
+echo 📥 下载 → 其它工具 \ Bili23-Downloade_v2.00.7_portable.zip
+curl -L -# -o "其它工具\Bili23-Downloade_v2.00.7_portable.zip" "https://gh-proxy.com/https://github.com/ScottSloan/Bili23-Downloader/releases/download/v2.00.7/Bili23-Downloader_2.00.7_windows_x64_portable.zip"
 echo.
 mkdir "浏览器相关" 2>nul
 echo 📥 下载 → 浏览器相关 \ Chrome++_v1.16.2_x86_x64_arm64.7z
 curl -L -# -o "浏览器相关\Chrome++_v1.16.2_x86_x64_arm64.7z" "https://gh-proxy.com/https://github.com/Bush2021/chrome_plus/releases/download/1.16.2/Chrome%2B%2B_v1.16.2_x86_x64_arm64.7z"
 echo.
-mkdir "运行库" 2>nul
-echo 📥 下载 → 运行库 \ dotnet-runtime-8.0.27-win-x64.exe
-curl -L -# -o "运行库\dotnet-runtime-8.0.27-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/8.0.27/dotnet-runtime-8.0.27-win-x64.exe"
+mkdir "浏览器相关" 2>nul
+echo 📥 下载 → 浏览器相关 \ chrome_149.0.7827.54_uncompressed.exe
+curl -L -# -o "浏览器相关\chrome_149.0.7827.54_uncompressed.exe" "https://gh-proxy.com/https://github.com/Bush2021/chrome_installer/releases/download/149.0.7827.54/x64_149.0.7827.54_chrome_installer_uncompressed.exe"
 echo.
-mkdir "文本办公" 2>nul
-echo 📥 下载 → 文本办公 \ EmEditor_portable.zip
-curl -L -# -o "文本办公\EmEditor_portable.zip" "https://download.emeditor.info/emed64_24.1.0_portable.zip"
+mkdir "其它工具" 2>nul
+echo 📥 下载 → 其它工具 \ Clash.Verge_2.5.1_x64-setup.exe
+curl -L -# -o "其它工具\Clash.Verge_2.5.1_x64-setup.exe" "https://gh-proxy.com/https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.5.1/Clash.Verge_2.5.1_x64-setup.exe"
+echo.
+mkdir "单文件" 2>nul
+echo 📥 下载 → 单文件 \ ClickMonitorDDC.zip
+curl -L -# -o "单文件\ClickMonitorDDC.zip" "https://gh-proxy.com/https://github.com/hintryer/ra/releases/download/2.0/ClickMonitorDDC.7.2.zip"
+echo.
+mkdir "运行库" 2>nul
+echo 📥 下载 → 运行库 \ dotnet-runtime-10.0.8-win-x64.exe
+curl -L -# -o "运行库\dotnet-runtime-10.0.8-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/10.0.8/dotnet-runtime-10.0.8-win-x64.exe"
 echo.
 mkdir "运行库" 2>nul
 echo 📥 下载 → 运行库 \ dotnet-runtime-6.0.36-win-x64.exe
 curl -L -# -o "运行库\dotnet-runtime-6.0.36-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/6.0.36/dotnet-runtime-6.0.36-win-x64.exe"
 echo.
-mkdir "文本办公" 2>nul
-echo 📥 下载 → 文本办公 \ Notepad4_HD_zh-Hans_x64_v26.05r6166.zip
-curl -L -# -o "文本办公\Notepad4_HD_zh-Hans_x64_v26.05r6166.zip" "https://gh-proxy.com/https://github.com/zufuliu/notepad4/releases/download/v26.05r6166/Notepad4_HD_zh-Hans_x64_v26.05r6166.zip"
-echo.
 mkdir "运行库" 2>nul
 echo 📥 下载 → 运行库 \ dotnet-runtime-7.0.20-win-x64.exe
 curl -L -# -o "运行库\dotnet-runtime-7.0.20-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/7.0.20/dotnet-runtime-7.0.20-win-x64.exe"
+echo.
+mkdir "运行库" 2>nul
+echo 📥 下载 → 运行库 \ dotnet-runtime-8.0.27-win-x64.exe
+curl -L -# -o "运行库\dotnet-runtime-8.0.27-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/8.0.27/dotnet-runtime-8.0.27-win-x64.exe"
+echo.
+mkdir "运行库" 2>nul
+echo 📥 下载 → 运行库 \ dotnet-runtime-9.0.16-win-x64.exe
+curl -L -# -o "运行库\dotnet-runtime-9.0.16-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/9.0.16/dotnet-runtime-9.0.16-win-x64.exe"
+echo.
+mkdir "文本办公" 2>nul
+echo 📥 下载 → 文本办公 \ EmEditor_portable.zip
+curl -L -# -o "文本办公\EmEditor_portable.zip" "https://download.emeditor.info/emed64_24.1.0_portable.zip"
+echo.
+mkdir "影音工具" 2>nul
+echo 📥 下载 → 影音工具 \ FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip
+curl -L -# -o "影音工具\FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip" "https://gh-proxy.com/https://github.com/eibols/ffmpeg_batch/releases/download/3.2.9/FFmpeg_Batch_AV_Converter_Portable_3.2.9_x64.zip"
+echo.
+mkdir "开发工具" 2>nul
+echo 📥 下载 → 开发工具 \ ffmpeg-8.1.1-essentials_build.7z
+curl -L -# -o "开发工具\ffmpeg-8.1.1-essentials_build.7z" "https://gh-proxy.com/https://github.com/GyanD/codexffmpeg/releases/download/8.1.1/ffmpeg-8.1.1-essentials_build.7z"
+echo.
+mkdir "开发工具" 2>nul
+echo 📥 下载 → 开发工具 \ ffmpeg-8.1.1-full_build.7z
+curl -L -# -o "开发工具\ffmpeg-8.1.1-full_build.7z" "https://gh-proxy.com/https://github.com/GyanD/codexffmpeg/releases/download/8.1.1/ffmpeg-8.1.1-full_build.7z"
 echo.
 mkdir "图像相关" 2>nul
 echo 📥 下载 → 图像相关 \ FSCapture_11.2.zip
 curl -L -# -o "图像相关\FSCapture_11.2.zip" "https://www.faststonesoft.net/DN/FSCapture112.zip"
 echo.
-mkdir "浏览器相关" 2>nul
-echo 📥 下载 → 浏览器相关 \ AllowCopy_2.1.6.crx
-curl -L -# -o "浏览器相关\AllowCopy_2.1.6.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Djhodmcfehicjonjdoljadkbkenaoehfm%26uc"
+mkdir "图像相关" 2>nul
+echo 📥 下载 → 图像相关 \ FSViewer_8.4.exe
+curl -L -# -o "图像相关\FSViewer_8.4.exe" "https://www.faststonesoft.net/DN/FSViewerSetup84.exe"
 echo.
-mkdir "影音工具" 2>nul
-echo 📥 下载 → 影音工具 \ Scribe2SRT-v0.3.6-windows-x86_64.zip
-curl -L -# -o "影音工具\Scribe2SRT-v0.3.6-windows-x86_64.zip" "https://gh-proxy.com/https://github.com/cylind/scribe2srt/releases/download/v0.3.6/Scribe2SRT-v0.3.6-windows-x86_64.zip"
-echo.
-mkdir "其它工具" 2>nul
-echo 📥 下载 → 其它工具 \ Steamcommunity_302_AMD64_V14.0.02.zip
-curl -L -# -o "其它工具\Steamcommunity_302_AMD64_V14.0.02.zip" "https://www.dogfight360.com/blog/wp-content/uploads/2026/02/steamcommunity_302_AMD64_V14.0.02.zip"
+mkdir "单文件" 2>nul
+echo 📥 下载 → 单文件 \ GeekUninstaller_1.5.3.170.zip
+curl -L -# -o "单文件\GeekUninstaller_1.5.3.170.zip" "https://geekuninstaller.com/geek.zip"
 echo.
 mkdir "下载传输" 2>nul
 echo 📥 下载 → 下载传输 \ lanzouyun-3.5.3-x64-win.zip
@@ -61,133 +85,109 @@ mkdir "浏览器相关" 2>nul
 echo 📥 下载 → 浏览器相关 \ LastPass_4.153.1.crx
 curl -L -# -o "浏览器相关\LastPass_4.153.1.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dhdokiejnpimakedhajhdlcegeplioahd%26uc"
 echo.
-mkdir "运行库" 2>nul
-echo 📥 下载 → 运行库 \ dotnet-runtime-9.0.16-win-x64.exe
-curl -L -# -o "运行库\dotnet-runtime-9.0.16-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/9.0.16/dotnet-runtime-9.0.16-win-x64.exe"
-echo.
-mkdir "开发工具" 2>nul
-echo 📥 下载 → 开发工具 \ ffmpeg-8.1.1-full_build.7z
-curl -L -# -o "开发工具\ffmpeg-8.1.1-full_build.7z" "https://gh-proxy.com/https://github.com/GyanD/codexffmpeg/releases/download/8.1.1/ffmpeg-8.1.1-full_build.7z"
-echo.
-mkdir "文本办公" 2>nul
-echo 📥 下载 → 文本办公 \ typora-plugin@v1.18.2.zip
-curl -L -# -o "文本办公\typora-plugin@v1.18.2.zip" "https://gh-proxy.com/https://github.com/obgnail/typora_plugin/releases/download/1.18.2/typora-plugin%40v1.18.2.zip"
+mkdir "下载传输" 2>nul
+echo 📥 下载 → 下载传输 \ LocalSend-1.17.0-windows-x86-64.zip
+curl -L -# -o "下载传输\LocalSend-1.17.0-windows-x86-64.zip" "https://gh-proxy.com/https://github.com/localsend/localsend/releases/download/v1.17.0/LocalSend-1.17.0-windows-x86-64.zip"
 echo.
 mkdir "单文件" 2>nul
-echo 📥 下载 → 单文件 \ GeekUninstaller_1.5.3.170.zip
-curl -L -# -o "单文件\GeekUninstaller_1.5.3.170.zip" "https://geekuninstaller.com/geek.zip"
+echo 📥 下载 → 单文件 \ MouseClickTool.exe
+curl -L -# -o "单文件\MouseClickTool.exe" "https://gh-proxy.com/https://github.com/lalakii/MouseClickTool/releases/download/3.1.0.0/MouseClickTool.exe"
+echo.
+mkdir "影音工具" 2>nul
+echo 📥 下载 → 影音工具 \ MusicPlayer2_V2.78_x64.zip
+curl -L -# -o "影音工具\MusicPlayer2_V2.78_x64.zip" "https://gh-proxy.com/https://github.com/zhongyang219/MusicPlayer2/releases/download/V2.78/MusicPlayer2_V2.78_x64.zip"
+echo.
+mkdir "影音工具" 2>nul
+echo 📥 下载 → 影音工具 \ MusicTag.zip
+curl -L -# -o "影音工具\MusicTag.zip" "https://gh-proxy.com/https://github.com/hintryer/ra/releases/download/2.0/MusicTag_v1.0.9.0.zip"
+echo.
+mkdir "文本办公" 2>nul
+echo 📥 下载 → 文本办公 \ Notepad4_HD_zh-Hans_x64_v26.05r6166.zip
+curl -L -# -o "文本办公\Notepad4_HD_zh-Hans_x64_v26.05r6166.zip" "https://gh-proxy.com/https://github.com/zufuliu/notepad4/releases/download/v26.05r6166/Notepad4_HD_zh-Hans_x64_v26.05r6166.zip"
+echo.
+mkdir "开发工具" 2>nul
+echo 📥 下载 → 开发工具 \ PortableGit-2.54.0-64-bit.7z.exe
+curl -L -# -o "开发工具\PortableGit-2.54.0-64-bit.7z.exe" "https://gh-proxy.com/https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/PortableGit-2.54.0-64-bit.7z.exe"
+echo.
+mkdir "浏览器相关" 2>nul
+echo 📥 下载 → 浏览器相关 \ SwitchyOmega_3.5.0.crx
+curl -L -# -o "浏览器相关\SwitchyOmega_3.5.0.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dpfnededegaaopdmhkdmcofjmoldfiped%26uc"
 echo.
 mkdir "文本办公" 2>nul
 echo 📥 下载 → 文本办公 \ QQWubi_Setup_2.4.629.400.exe
 curl -L -# -o "文本办公\QQWubi_Setup_2.4.629.400.exe" "https://qq.pinyin.cn/download_pc.php?t=wb"
 echo.
-mkdir "其它工具" 2>nul
-echo 📥 下载 → 其它工具 \ Clash.Verge_2.5.1_x64-setup.exe
-curl -L -# -o "其它工具\Clash.Verge_2.5.1_x64-setup.exe" "https://gh-proxy.com/https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.5.1/Clash.Verge_2.5.1_x64-setup.exe"
-echo.
-mkdir "文本办公" 2>nul
-echo 📥 下载 → 文本办公 \ app_asar_file_v1.9.5.zip
-curl -L -# -o "文本办公\app_asar_file_v1.9.5.zip" "https://gh-proxy.com/https://github.com/Delppine1024/TGreen/releases/download/v1.9.5/app_asar_file_v1.9.5.zip"
+mkdir "单文件" 2>nul
+echo 📥 下载 → 单文件 \ ReNamerPro_7.9.0.0v2.exe
+curl -L -# -o "单文件\ReNamerPro_7.9.0.0v2.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQnYyUzBVdzNGeFI2M3A4QWU4d1BYb0FiN09HY1RQYWsteEwxRmxocDBYcjM0P2U9SXJodUhu.exe"
 echo.
 mkdir "影音工具" 2>nul
-echo 📥 下载 → 影音工具 \ РotРlayer-260422(1.7.22859)-x64-Stable.exe
-curl -L -# -o "影音工具\РotРlayer-260422(1.7.22859)-x64-Stable.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQlhNb0lHNk9HLVJyb0lLS3cxMHlaSUFVOV9CR2FvVlVRanF4TlBISEMtWXJrP2U9c01TelJk.exe"
+echo 📥 下载 → 影音工具 \ Scribe2SRT-v0.3.6-windows-x86_64.zip
+curl -L -# -o "影音工具\Scribe2SRT-v0.3.6-windows-x86_64.zip" "https://gh-proxy.com/https://github.com/cylind/scribe2srt/releases/download/v0.3.6/Scribe2SRT-v0.3.6-windows-x86_64.zip"
 echo.
 mkdir "浏览器相关" 2>nul
 echo 📥 下载 → 浏览器相关 \ SingleFile_1.22.98.crx
 curl -L -# -o "浏览器相关\SingleFile_1.22.98.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dmpiodijhokgodhhofbcjdecpffjipkle%26uc"
 echo.
 mkdir "其它工具" 2>nul
-echo 📥 下载 → 其它工具 \ 360FileSetup_1.0.0.1180.exe
-curl -L -# -o "其它工具\360FileSetup_1.0.0.1180.exe" "https://sfdl.360safe.com/pcnewapp/360FileSetup.exe?channel=9510140"
+echo 📥 下载 → 其它工具 \ Steamcommunity_302_AMD64_V14.0.02.zip
+curl -L -# -o "其它工具\Steamcommunity_302_AMD64_V14.0.02.zip" "https://www.dogfight360.com/blog/wp-content/uploads/2026/02/steamcommunity_302_AMD64_V14.0.02.zip"
 echo.
-mkdir "开发工具" 2>nul
-echo 📥 下载 → 开发工具 \ PortableGit-2.54.0-64-bit.7z.exe
-curl -L -# -o "开发工具\PortableGit-2.54.0-64-bit.7z.exe" "https://gh-proxy.com/https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/PortableGit-2.54.0-64-bit.7z.exe"
+mkdir "下载传输" 2>nul
+echo 📥 下载 → 下载传输 \ Thunder_12.4.9.3918.exe
+curl -L -# -o "下载传输\Thunder_12.4.9.3918.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQ0NCVS1wQ0RPTFM2amt2NXNJVmJmYUFlaEhMbk1zOHItUjBxbDB2c1VXTHFVP2U9eGVsZnhJ.exe"
 echo.
 mkdir "文本办公" 2>nul
 echo 📥 下载 → 文本办公 \ typora-windows-x64-1.9.5.exe
 curl -L -# -o "文本办公\typora-windows-x64-1.9.5.exe" "https://gh-proxy.com/https://github.com/Delppine1024/TGreen/releases/download/v1.9.5/typora-windows-x64-1.9.5.exe"
+echo.
+mkdir "文本办公" 2>nul
+echo 📥 下载 → 文本办公 \ app_asar_file_v1.9.5.zip
+curl -L -# -o "文本办公\app_asar_file_v1.9.5.zip" "https://gh-proxy.com/https://github.com/Delppine1024/TGreen/releases/download/v1.9.5/app_asar_file_v1.9.5.zip"
+echo.
+mkdir "文本办公" 2>nul
+echo 📥 下载 → 文本办公 \ typora-plugin@v1.18.2.zip
+curl -L -# -o "文本办公\typora-plugin@v1.18.2.zip" "https://gh-proxy.com/https://github.com/obgnail/typora_plugin/releases/download/1.18.2/typora-plugin%40v1.18.2.zip"
+echo.
+mkdir "浏览器相关" 2>nul
+echo 📥 下载 → 浏览器相关 \ uBlockOriginLite_2026.529.1448.crx
+curl -L -# -o "浏览器相关\uBlockOriginLite_2026.529.1448.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dddkjiahejlhfcafbddmgiahcphecmpfh%26uc"
+echo.
+mkdir "图像相关" 2>nul
+echo 📥 下载 → 图像相关 \ Umi-OCR_Paddle_v2.1.5.7z.exe
+curl -L -# -o "图像相关\Umi-OCR_Paddle_v2.1.5.7z.exe" "https://gh-proxy.com/https://github.com/hiroi-sora/Umi-OCR/releases/download/v2.1.5/Umi-OCR_Paddle_v2.1.5.7z.exe"
+echo.
+mkdir "影音工具" 2>nul
+echo 📥 下载 → 影音工具 \ VideoCaptioner-Setup-win64-v1.3.3.exe
+curl -L -# -o "影音工具\VideoCaptioner-Setup-win64-v1.3.3.exe" "https://gh-proxy.com/https://github.com/WEIFENG2333/VideoCaptioner/releases/download/v1.3.3/VideoCaptioner-Setup-win64-v1.3.3.exe"
+echo.
+mkdir "运行库" 2>nul
+echo 📥 下载 → 运行库 \ All_VC_2026.04.16.exe
+curl -L -# -o "运行库\All_VC_2026.04.16.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQWtFanNtZ1J6cFM2S3dxRTNBOGhrekFjZHFsN19adTZtVTFCTVM2eVF3QVA0P2U9Q0UzOVpW.exe"
+echo.
+mkdir "影音工具" 2>nul
+echo 📥 下载 → 影音工具 \ PotРlayer-260422.exe
+curl -L -# -o "影音工具\PotРlayer-260422.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQlhNb0lHNk9HLVJyb0lLS3cxMHlaSUFVOV9CR2FvVlVRanF4TlBISEMtWXJrP2U9c01TelJk.exe"
+echo.
+mkdir "浏览器相关" 2>nul
+echo 📥 下载 → 浏览器相关 \ Immersive_Translate_1.29.6.crx
+curl -L -# -o "浏览器相关\Immersive_Translate_1.29.6.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dbpoadfkcbjbfhfodiogcnhhhpibjhbnh%26uc"
+echo.
+mkdir "浏览器相关" 2>nul
+echo 📥 下载 → 浏览器相关 \ cat-catch_2.6.9.crx
+curl -L -# -o "浏览器相关\cat-catch_2.6.9.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Djfedfbgedapdagkghmgibemcoggfppbb%26uc"
 echo.
 mkdir "浏览器相关" 2>nul
 echo 📥 下载 → 浏览器相关 \ Tampermonkey_5.5.6237.crx
 curl -L -# -o "浏览器相关\Tampermonkey_5.5.6237.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Ddhdgffkkebhmkfjojejmpbldmpobfkfo%26uc"
 echo.
 mkdir "浏览器相关" 2>nul
-echo 📥 下载 → 浏览器相关 \ SwitchyOmega_3.5.0.crx
-curl -L -# -o "浏览器相关\SwitchyOmega_3.5.0.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dpfnededegaaopdmhkdmcofjmoldfiped%26uc"
-echo.
-mkdir "单文件" 2>nul
-echo 📥 下载 → 单文件 \ ClickMonitorDDC.zip
-curl -L -# -o "单文件\ClickMonitorDDC.zip" "https://gh-proxy.com/https://github.com/hintryer/ra/releases/download/2.0/ClickMonitorDDC.7.2.zip"
+echo 📥 下载 → 浏览器相关 \ AllowCopy_2.1.6.crx
+curl -L -# -o "浏览器相关\AllowCopy_2.1.6.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Djhodmcfehicjonjdoljadkbkenaoehfm%26uc"
 echo.
 mkdir "浏览器相关" 2>nul
 echo 📥 下载 → 浏览器相关 \ SuperDrag_2.1.2.crx
 curl -L -# -o "浏览器相关\SuperDrag_2.1.2.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Djaikcnhlohebodlpkmjepipngegjbfpg%26uc"
-echo.
-mkdir "下载传输" 2>nul
-echo 📥 下载 → 下载传输 \ LocalSend-1.17.0-windows-x86-64.zip
-curl -L -# -o "下载传输\LocalSend-1.17.0-windows-x86-64.zip" "https://gh-proxy.com/https://github.com/localsend/localsend/releases/download/v1.17.0/LocalSend-1.17.0-windows-x86-64.zip"
-echo.
-mkdir "开发工具" 2>nul
-echo 📥 下载 → 开发工具 \ ffmpeg-8.1.1-essentials_build.7z
-curl -L -# -o "开发工具\ffmpeg-8.1.1-essentials_build.7z" "https://gh-proxy.com/https://github.com/GyanD/codexffmpeg/releases/download/8.1.1/ffmpeg-8.1.1-essentials_build.7z"
-echo.
-mkdir "浏览器相关" 2>nul
-echo 📥 下载 → 浏览器相关 \ Immersive_Translate_1.29.6.crx
-curl -L -# -o "浏览器相关\Immersive_Translate_1.29.6.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dbpoadfkcbjbfhfodiogcnhhhpibjhbnh%26uc"
-echo.
-mkdir "单文件" 2>nul
-echo 📥 下载 → 单文件 \ MouseClickTool.exe
-curl -L -# -o "单文件\MouseClickTool.exe" "https://gh-proxy.com/https://github.com/lalakii/MouseClickTool/releases/download/3.1.0.0/MouseClickTool.exe"
-echo.
-mkdir "浏览器相关" 2>nul
-echo 📥 下载 → 浏览器相关 \ uBlockOriginLite_2026.529.1448.crx
-curl -L -# -o "浏览器相关\uBlockOriginLite_2026.529.1448.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Dddkjiahejlhfcafbddmgiahcphecmpfh%26uc"
-echo.
-mkdir "影音工具" 2>nul
-echo 📥 下载 → 影音工具 \ MusicPlayer2_V2.78_x64.zip
-curl -L -# -o "影音工具\MusicPlayer2_V2.78_x64.zip" "https://gh-proxy.com/https://github.com/zhongyang219/MusicPlayer2/releases/download/V2.78/MusicPlayer2_V2.78_x64.zip"
-echo.
-mkdir "浏览器相关" 2>nul
-echo 📥 下载 → 浏览器相关 \ chrome_149.0.7827.54_uncompressed.exe
-curl -L -# -o "浏览器相关\chrome_149.0.7827.54_uncompressed.exe" "https://gh-proxy.com/https://github.com/Bush2021/chrome_installer/releases/download/149.0.7827.54/x64_149.0.7827.54_chrome_installer_uncompressed.exe"
-echo.
-mkdir "图像相关" 2>nul
-echo 📥 下载 → 图像相关 \ Umi-OCR_Paddle_v2.1.5.7z.exe
-curl -L -# -o "图像相关\Umi-OCR_Paddle_v2.1.5.7z.exe" "https://gh-proxy.com/https://github.com/hiroi-sora/Umi-OCR/releases/download/v2.1.5/Umi-OCR_Paddle_v2.1.5.7z.exe"
-echo.
-mkdir "下载传输" 2>nul
-echo 📥 下载 → 下载传输 \ Thunder_12.4.9.3918.exe
-curl -L -# -o "下载传输\Thunder_12.4.9.3918.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQ0NCVS1wQ0RPTFM2amt2NXNJVmJmYUFlaEhMbk1zOHItUjBxbDB2c1VXTHFVP2U9eGVsZnhJ.exe"
-echo.
-mkdir "单文件" 2>nul
-echo 📥 下载 → 单文件 \ ReNamerPro_7.9.0.0v2.exe
-curl -L -# -o "单文件\ReNamerPro_7.9.0.0v2.exe" "https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy9kMTk2Y2Y1YmZjNWM1YmY3L0lRQnYyUzBVdzNGeFI2M3A4QWU4d1BYb0FiN09HY1RQYWsteEwxRmxocDBYcjM0P2U9SXJodUhu.exe"
-echo.
-mkdir "浏览器相关" 2>nul
-echo 📥 下载 → 浏览器相关 \ cat-catch_2.6.9.crx
-curl -L -# -o "浏览器相关\cat-catch_2.6.9.crx" "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=130.0.0.0&x=id%3Djfedfbgedapdagkghmgibemcoggfppbb%26uc"
-echo.
-mkdir "影音工具" 2>nul
-echo 📥 下载 → 影音工具 \ MusicTag.zip
-curl -L -# -o "影音工具\MusicTag.zip" "https://gh-proxy.com/https://github.com/hintryer/ra/releases/download/2.0/MusicTag_v1.0.9.0.zip"
-echo.
-mkdir "运行库" 2>nul
-echo 📥 下载 → 运行库 \ dotnet-runtime-10.0.8-win-x64.exe
-curl -L -# -o "运行库\dotnet-runtime-10.0.8-win-x64.exe" "https://builds.dotnet.microsoft.com/dotnet/Runtime/10.0.8/dotnet-runtime-10.0.8-win-x64.exe"
-echo.
-mkdir "图像相关" 2>nul
-echo 📥 下载 → 图像相关 \ FSViewer_8.4.exe
-curl -L -# -o "图像相关\FSViewer_8.4.exe" "https://www.faststonesoft.net/DN/FSViewerSetup84.exe"
-echo.
-mkdir "影音工具" 2>nul
-echo 📥 下载 → 影音工具 \ VideoCaptioner-Setup-win64-v1.3.3.exe
-curl -L -# -o "影音工具\VideoCaptioner-Setup-win64-v1.3.3.exe" "https://gh-proxy.com/https://github.com/WEIFENG2333/VideoCaptioner/releases/download/v1.3.3/VideoCaptioner-Setup-win64-v1.3.3.exe"
-echo.
-mkdir "其它工具" 2>nul
-echo 📥 下载 → 其它工具 \ Bili23-Downloade_v2.00.7_portable.zip
-curl -L -# -o "其它工具\Bili23-Downloade_v2.00.7_portable.zip" "https://gh-proxy.com/https://github.com/ScottSloan/Bili23-Downloader/releases/download/v2.00.7/Bili23-Downloader_2.00.7_windows_x64_portable.zip"
 echo.
 echo ==============================================
 echo                 ✅ 全部下载完成
